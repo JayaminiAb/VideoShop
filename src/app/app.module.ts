257397a2c215dc987e06cefef5b8b96c5d67f844
JayaminiAb <54019249+JayaminiAb@users.noreply.github.com>
2021-06-17 17:59:07 +0530

    video-shop v1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule} from '@angular/router';
+import { MembersComponent } from './components/members/members.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieRentComponent } from './components/movie-rent/movie-rent.component';
+import { NameFilterPipe } from './pipes/name-filter.pipe';
+import { LanguageFilterPipe } from './pipes/language-filter.pipe';
+import {DatePipe} from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MembersComponent,
+    MoviesComponent,
+    MovieRentComponent,
+    NameFilterPipe,
+    LanguageFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
